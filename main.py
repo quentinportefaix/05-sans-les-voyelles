2356cc4ddd48b59ef3d30b0a0632787685792920
portefaq <quentin.portefaix@edu.esiee.fr>
2025-09-22 12:32:55 +0000

    Sansvoyelle

--- main.py
+++ main.py
@@ -1,22 +1,75 @@
-#### Fonction secondaire
+def remove_vowels_it(s):
+    """Retire les voyelles de la chaÃ®ne de caractÃ¨re passÃ©e en paramÃ¨tre
 
+    Args:
+        s (str): chaine de caractÃ¨re Ã  traiter
 
-def ispalindrome(p):
+    Returns:
+        str: chaine de caractÃ¨re privÃ©e de ses voyelles
 
-    # votre code ici
-    
-    return False
+    >>> s = "elephant"
+    >>> remove_vowels_it(s)
+    'lphnt'
+    >>> s = "crocodile"
+    >>> remove_vowels_it(s)
+    'crcdl'
+    >>> s = "girafe"
+    >>> remove_vowels_it(s)
+    'grf'
+    >>> s = "phacochere"
+    >>> remove_vowels_it(s)
+    'phcchr'
+    >>> s = "ornithorynque"
+    >>> remove_vowels_it(s)
+    'rnthrnq'
+    """
+    out = ""
+    for c in s:
+        if c in "aeiouy": continue
+        out += c
+    return out
 
-#### Fonction principale
+def remove_vowels_rec(s):
+    """Retire les voyelles de la chaÃ®ne de caractÃ¨re passÃ©e en paramÃ¨tre
 
+    Args:
+        s (str): chaine de caractÃ¨re Ã  traiter
+
+    Returns:
+        str: chaine de caractÃ¨re privÃ©e de ses voyelles
+
+    >>> s = "elephant"
+    >>> remove_vowels_rec(s)
+    'lphnt'
+    >>> s = "crocodile"
+    >>> remove_vowels_rec(s)
+    'crcdl'
+    >>> s = "girafe"
+    >>> remove_vowels_rec(s)
+    'grf'
+    >>> s = "phacochere"
+    >>> remove_vowels_rec(s)
+    'phcchr'
+    >>> s = "ornithorynque"
+    >>> remove_vowels_rec(s)
+    'rnthrnq'
+    """
+    # base case
+
+    # recursive call si s starts with a vowel
+    if s=="" :
+        return ""
+    if s[0] in "aeiouy" :
+
+        return remove_vowels_rec(s[1:])
+    # recursive call if s starts with a consonant
+
+    return s[0] + remove_vowels_rec(s[1:])
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
-        print(s, ispalindrome(s))
-
+    pass
+    s = "Florent"
+    print(remove_vowels_rec(s))
 
 if __name__ == "__main__":
     main()
